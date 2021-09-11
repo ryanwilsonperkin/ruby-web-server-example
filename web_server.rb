bea2c578e2a845b4844020c8af76153bd9b559ed
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-09-10 20:42:38 -0400

    Introduce a web application
    At this point its worth our time to begin talking about the difference
    between a "web server" and a "web application". For our purposes, we'll
    consider the "web server" the part that's responsible for receiving HTTP
    and responding with it, while the "web application" will think in terms
    of request and response objects, and can introduce its own logic for
    routing those requests.
    
    We'll start quite simple, our WebApplication has a process route which
    the WebServer will call with a request. Within the WebApplication, we
    can do whatever we want with that request, so long as we return a
    special response format, which we'll consider to be an array consisting
    of the status code, headers, and a body. (More on why we chose this
    format later).
    
    Since the WebApplication will be handing a numeric status code back to
    the WebServer, we'll also have to add a bit of logic to the WebServer to
    know how to map this back to a "Reason Phrase" which the HTTP spec
    expects us to include. For now we'll just focus on the status codes "200
    Ok" and "404 Not Found". A complete list can be found in the HTTP
    response spec.

--- web_server.rb
+++ web_server.rb
@@ -1,12 +1,18 @@
 require 'socket'
+require_relative 'web_application'
 
 class WebServer
   HOST = '127.0.0.1'
   PORT = 8000
   MAX_REQUEST_SIZE = 1024
+  STATUS_REASON_PHRASES = {
+    200 => 'OK',
+    404 => 'Not Found'
+  }
 
   def initialize
     @server = TCPServer.new HOST, PORT
+    @web_application = WebApplication.new
   end
 
   # We'll convert the raw HTTP string into a series of lines
@@ -63,14 +69,14 @@ class WebServer
   # Most browsers will simply print this body out if it is text,
   # which is what we use in this example. HTML and other formats can
   # also be used here by setting the appropriate Content-Type header.
-  def prepare_response(request)
-    response_body = "Hello World!\n#{request}"
+  def prepare_response(status, headers, body)
+    status_reason_phrase = STATUS_REASON_PHRASES[status]
+    header_lines = headers.map { |key, value| "#{key}: #{value}" }
     <<~EOF
-      HTTP/1.1 200 OK
-      Content-Type: text/plain
-      Content-Length: #{response_body.length}
+      HTTP/1.1 #{status} #{status_reason_phrase}
+      #{header_lines.join("\n")}
 
-      #{response_body}
+      #{body}
     EOF
   end
 
@@ -83,7 +89,8 @@ class WebServer
       request_http = connection.recv MAX_REQUEST_SIZE
       request = parse_request(request_http)
       puts request
-      connection.puts prepare_response(request)
+      status, headers, body = @web_application.process(request)
+      connection.puts prepare_response(status, headers, body)
       connection.close
     end
   end
