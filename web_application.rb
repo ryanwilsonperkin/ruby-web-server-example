4dc8f773c213be6d86eb4784013feeec725dba0a
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2021-09-10 21:22:36 -0400

    Introducing better Request/Response objects
    Every good web application should probably have a well-defined Request
    and Response object. We're creating a couple to wrap the request hash
    and response array that we use to communicate with our WebApplication
    which makes the route methods in our WebServer much nicer to work with:
    they get a Request which has some well-defined methods and they can
    return a Response.
    
    By creating these objects, we can also start to cut down on some
    repitition. The route methods no longer need to calculate the body
    length or specify the content type, nor do they have to specify the
    status code unless they want to deviate from the 200 default.

--- web_application.rb
+++ web_application.rb
@@ -1,4 +1,61 @@
 class WebApplication
+
+  class Request
+    def initialize(request_hash)
+      @request_hash = request_hash
+    end
+
+    def version
+      @request_hash['version'].freeze
+    end
+
+    def method
+      @request_hash['method'].freeze
+    end
+
+    def path
+      @request_hash['path'].freeze
+    end
+
+    def headers
+      @request_hash['headers'].freeze
+    end
+
+    def body
+      @request_hash['body'].freeze
+    end
+
+    def to_s
+      <<~EOF
+        Version: #{version}
+        Method: #{method}
+        Path: #{path}
+        Headers: #{headers}
+        Body: #{body}
+      EOF
+    end
+  end
+
+  class Response
+    attr_reader :body, :status
+
+    def initialize(body, status: 200)
+      @body = body
+      @status = status
+    end
+
+    def headers
+      {
+        'Content-Length' => @body.length,
+        'Content-Type' => 'text/plain'
+      }
+    end
+
+    def to_ary
+      [status, headers, body]
+    end
+  end
+
   # Receive a request from the WebServer and route it appropriately
   # Based on the path that was received, we'll forward to one of our
   # *_route methods which will respond with a specific format.
@@ -8,36 +65,28 @@ class WebApplication
   # object (a hash with specific keys) in the WebServer. Now we'll
   # define the response format here which will be an array with three
   # values: an integer status code, a hash of headers, and a string body.
-  def process(request)
-    case request['path']
+  def process(request_hash)
+    request = Request.new(request_hash)
+    case request.path
     when '/'
       index_route(request)
-    when %r{/\d+}
-      number_route(request)
+    when %r{^/sleep/\d+$}
+      sleep_route(request)
     else
-      not_found_route(request)
+      Response.new(
+        "No route found for #{request.path}. Try '/' or '/sleep/3'.",
+        status: 404
+      )
     end
   end
 
   def index_route(request)
-    status = 200
-    body = "Hello World! #{request}"
-    headers = { 'Content-Type': 'text/plain', 'Content-Length': body.length }
-    [status, headers, body]
+    Response.new("Hello World!\n#{request}")
   end
 
-  def number_route(request)
-    number = request['path'].delete_prefix('/')
-    status = 200
-    body = "You asked for the number #{number}"
-    headers = { 'Content-Type': 'text/plain', 'Content-Length': body.length }
-    [status, headers, body]
-  end
-
-  def not_found_route(request)
-    status = 404
-    body = "No route found for #{request['path']}. Try '/' or a number like '/123'."
-    headers = { 'Content-Type': 'text/plain', 'Content-Length': body.length }
-    [status, headers, body]
+  def sleep_route(request)
+    seconds = request.path.delete_prefix('/sleep/').to_i
+    sleep seconds
+    Response.new("It took #{seconds}s to respond to this request!")
   end
 end
